56883896a40fce209921909a1a1a9ae42bed23e8
Damian F <damian.fikier@wp.pl>
2019-05-30 19:50:16 +0200

    removed ignored files

---
+++ 2DChart/Data/Models/ChaFun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2DChart.Data.Models
+{
+    public partial class ChaFun
+    {
+        public Guid ChaId { get; set; }
+        public Guid FunId { get; set; }
+
+        public virtual Chart Cha { get; set; }
+        public virtual Function Fun { get; set; }
+    }
+}
---
+++ 2DChart/Data/Models/Chart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2DChart.Data.Models
+{
+    public partial class Chart
+    {
+        public Chart()
+        {
+            ChaFun = new HashSet<ChaFun>();
+            ChaRep = new HashSet<ChaRep>();
+        }
+
+        public Guid ChartId { get; set; }
+        public string Logo { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public virtual ICollection<ChaFun> ChaFun { get; set; }
+        public virtual ICollection<ChaRep> ChaRep { get; set; }
+    }
+}
