819f7b8f80a2a51c31ec3818b10f1d79919a4910
Jakub Kucharz <ruseknobusek@wp.pl>
2019-05-31 13:55:30 +0200

    Equation

--- 2DChart/Domain/EquationMenager/Equation.cs
+++ 2DChart/Domain/EquationMenager/Equation.cs
@@ -11,14 +11,14 @@ namespace _2DChart.Domain.EquationMenager
         Expression expression;
         double x;
         Data.Models.Function function;
-        FunctionString functionString;
+        string functionString;
 
-        public Equation(double x, Data.Models.Function function, FunctionString functionString)
+        public Equation(double x, Data.Models.Function function)
         {
             this.x = x;
             this.function = function;
-            this.functionString = functionString;
-            expression.setExpressionString(functionString.FString);            
+            SetFunctionString(function.Parameters);
+            expression.setExpressionString(functionString);            
         }
 
         public double GetFunctionValue(double x)
@@ -26,6 +26,18 @@ namespace _2DChart.Domain.EquationMenager
             expression.setArgumentValue("x", x);
             return expression.calculate();
 
+        }
+
+        public void SetFunctionString(ICollection<Data.Models.Parameter> parameters)
+        {
+            functionString = "y = ";
+            foreach (Data.Models.Parameter parameter in parameters)
+            {
+                if (parameter.Value < 0 || functionString == "y = ")
+                    functionString += parameter.Value + " * " + parameter.Variable;
+                else
+                    functionString += " + " + parameter.Value + " * " + parameter.Variable;
+            }         
         }
 
     }
